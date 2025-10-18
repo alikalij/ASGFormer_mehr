28ddc15d85214fe71c62a035e2e280f93d5cb968
Ali Kalij <ali.kalij@gmail.com>
2025-10-18 14:15:20 +0330

    change dataset config

--- configs/env_config.py
+++ configs/env_config.py
@@ -28,12 +28,12 @@ BASE_CONFIG = {
 # تنظیمات خاص محیط
 ENV_SPECIFIC = {
     'colab': {
-        'dataset_path': '/content/dataset/s3dis-mini',
+        'dataset_path': '/content/dataset/s3dis',
         'checkpoint_dir': '/content/drive/MyDrive/saved_models/',
         'requirements': True
     },
     'local': {
-        'dataset_path': str(Path(__file__).parent.parent / 'data/s3dis-mini'),
+        'dataset_path': str(Path(__file__).parent.parent / 'data/s3dis'),
         'checkpoint_dir': str(Path(__file__).parent.parent / 'saved_models/'),
         'requirements': False
     }
